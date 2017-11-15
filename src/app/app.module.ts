ebaa1749280365a9c2580d359e5afe44102ab897
LuisenKtt <kttweb@gmail.com>
2017-11-15 18:41:47 +0100

    Servicio creado, header, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpModule } from '@angular/http';
+
 // Rutas
 import { APP_ROUTING } from './app.routes';
+
+// Servicios
+import { InformacionService } from './providers/informacion.service';
 
 
 import { AppComponent } from './app.component';
@@ -23,9 +28,12 @@ import { ProductoComponent } from './components/producto/producto.component';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    InformacionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
