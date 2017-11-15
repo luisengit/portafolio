ecef93dd15af37eb61c71d9ecbe5ceaa20874cf7
LuisenKtt <kttweb@gmail.com>
2017-11-15 12:53:42 +0100

    Header, Body, Footer listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './commponents/header/header.component';
+import { FooterComponent } from './commponents/footer/footer.component';
+import { PortafolioComponent } from './commponents/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule
