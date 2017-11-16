154d4f776c70155d0bf0fa5fcbf67f9942701949
LuisenKtt <kttweb@gmail.com>
2017-11-16 11:19:35 +0100

    Servicio de Firebase listo

--- src/app/providers/informacion.service.ts
+++ src/app/providers/informacion.service.ts
@@ -6,16 +6,30 @@ export class InformacionService {
 
   info:any = {};
   cargada:boolean = false;
+  cargada_sobre_nosotros:boolean = false;
+  equipo:any[] = [];
 
   constructor( public http:Http ) {
+      this.carga_info();
+      this.carga_sobre_nosotros();
+   }
 
-    this.http.get("assets/data/info.pagina.json")
-              .subscribe( data => {
-                console.log(data.json());
-                this.cargada = true;
-                this.info = data.json();
-              } )
+   public carga_info(){
+     this.http.get("assets/data/info.pagina.json")
+               .subscribe( data => {
+                 // console.log(data.json());
+                 this.cargada = true;
+                 this.info = data.json();
+               });
+   }
 
+   public carga_sobre_nosotros(){
+     this.http.get("https://paginaweb-1496b.firebaseio.com/equipo.json")
+               .subscribe( data => {
+                 console.log(data.json());
+                 this.cargada = true;
+                 this.equipo = data.json();
+               });
 
    }
 
