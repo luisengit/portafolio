179ae6eab2dfd0b595d6e16d4dadbae7753072af
LuisenKtt <kttweb@gmail.com>
2017-11-16 13:08:24 +0100

    Página de producto y ruta lista

--- src/app/providers/productos.service.ts
+++ src/app/providers/productos.service.ts
@@ -18,9 +18,12 @@ export class ProductosService {
 
       this.http.get('https://paginaweb-1496b.firebaseio.com/productos_idx.json')
           .subscribe( res => {
-            console.log(res.json());
-            this.cargando = false;
-            this.productos = res.json();
+            // console.log(res.json());
+
+            // setTimeout( ()=> {
+              this.cargando = false;
+              this.productos = res.json();
+            // }, 1500 )
           });
 
   }
